83224b1061280c1c9c190d8633e7f9c92f134581
kalenkevich <kalenkevich@google.com>
2026-01-27 12:23:40 -0800

    feat(@kalenkevich/test_release_workflow): add new feature X

--- core/src/index.ts
+++ core/src/index.ts
@@ -2,6 +2,11 @@ function someFunction() {
   return "Hello, World!";
 }
 
+function someOtherFunction() {
+  return "Goodbye, World!";
+}
+
 export function doHelloWorld() {
   console.log(someFunction());
+  console.log(someOtherFunction());
 }
