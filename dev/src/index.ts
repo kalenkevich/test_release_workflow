5539737f9b1f95900bdbdf3544621d5fda2bffcb
kalenkevich <kalenkevich@google.com>
2026-01-27 16:37:59 -0800

    fix: update dev to trigger release please.

--- dev/src/index.ts
+++ dev/src/index.ts
@@ -3,6 +3,7 @@ import {doHelloWorld} from '@kalenkevich/test_release_workflow';
 export function doHelloWorldDev() {
   const response = doHelloWorld();
 
+  response.goodAfternoon = 'test';
   console.log(response);
 
   return response;
